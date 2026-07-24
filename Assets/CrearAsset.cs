535ac402846027ee58548354fcd76c49b1e19519
matias-selzer <matias.selzer@gmail.com>
2026-07-23 23:36:57 -0300

    android

--- Assets/CrearAsset.cs
+++ Assets/CrearAsset.cs
@@ -15,4 +15,9 @@ public class CrearAsset : MonoBehaviour
             assetACrear.InstantiateAsync();
         }
     }
+
+    public void Crear()
+    {
+        assetACrear.InstantiateAsync();
+    }
 }
