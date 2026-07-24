4c42b29bc3d712baef949bd9cdc7f3476d9cff78
matias-selzer <matias.selzer@gmail.com>
2026-07-23 22:05:37 -0300

    Initial commit

---
+++ Assets/CrearAsset.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class CrearAsset : MonoBehaviour
+{
+    [SerializeField] AssetReferenceGameObject assetACrear;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            assetACrear.InstantiateAsync();
+        }
+    }
+}
